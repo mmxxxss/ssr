e36ac411ac80b32eb1179f3432ff8518ba533dd6
马晓帅 <maxs3@yonyou.com>
2025-09-09 14:37:24 +0800

    f

--- src/client/page/Index/index.js
+++ src/client/page/Index/index.js
@@ -4,22 +4,29 @@ import { removeToken } from '../../token';
 
 export default function Index({ history }) {
     const [account, setAccount] = useState({});
+    const [isLogin, setIsLogin] = useState(false);
     useEffect(() => {
         getUserAccount().then(res => {
             setAccount(res.data);
+            setIsLogin(true);
         }).catch(err => {
-            console.log(err, 'err');
+            if (err.response.status === 401) {
+                setIsLogin(false);
+            } else {
+                console.log(err, 'err');
+            }
         })
     }, [])
     const handleLogout = () => {
         removeToken();
+        setIsLogin(false);
         history.current.push('/login');
     };
     return (
         (
             <div className="index">
                 <div>欢迎, {account.username}!</div>
-                <button onClick={handleLogout}>退出登录</button>
+                <button onClick={handleLogout}>{isLogin ? '退出登录' : '登录'}</button>
             </div>
         )
     )
