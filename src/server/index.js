3232ee6f623e9a66ca6b40c5bb1334fab9d12601
马晓帅 <maxs3@yonyou.com>
2025-09-17 15:00:47 +0800

    ci/cd

--- src/server/index.js
+++ src/server/index.js
@@ -76,7 +76,7 @@ app.get('*', async (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
