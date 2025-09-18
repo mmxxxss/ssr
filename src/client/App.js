b72a0a3d8389114e064ee85120fd2ef5c3a005dc
马晓帅 <maxs3@yonyou.com>
2025-09-18 11:56:55 +0800

    feat:数据渲染

--- src/client/App.js
+++ src/client/App.js
@@ -3,18 +3,14 @@ import Login from './page/Login/login.js';
 import Index from './page/Index/index.js';
 import createHistory from './history.js';
 
-function App({ initialPath, userData = {} }) {
-  const [currentPath, setCurrentPath] = useState(() => {
-    if (initialPath !== undefined) {
-      return initialPath;
-    }
-    return '/ssr';
-  });
-  let history = useRef(null);
+function App({ initialPath = typeof window !== 'undefined' ? window.initialPath : '/', userData = {} }) {
+  const [currentPath, setCurrentPath] = useState(initialPath);
+  const history = useRef(null);
+  
   useEffect(() => {
     // 在客户端创建HistoryManager实例
     if (typeof window !== 'undefined') {
-      history.current = createHistory(window.initialPath);
+      history.current = createHistory(initialPath);
       // 订阅路由变化
       const unsubscribe = history.current.subscribe((path) => {
         setCurrentPath(path);
@@ -24,8 +20,9 @@ function App({ initialPath, userData = {} }) {
         unsubscribe();
       };
     }
-  }, []);
+  }, [initialPath]);
 
+  // 确保在hydration过程中，服务器端和客户端渲染的组件结构一致
   return (
     <div>
       {currentPath === '/ssr' && <Index history={history} userData={userData} />}
